5dffd4170d6b4e7803beea819d9e949f80a28b04
Piliwiwi <35037350+Piliwiwi@users.noreply.github.com>
2022-05-25 00:40:26 -0400

    feature: products mvi processor and reducer

---
+++ app/src/main/java/cl/arech/awesomeitems/products/presentation/list/ListProcessor.kt
@@ -0,0 +1,40 @@
+package cl.arech.awesomeitems.products.presentation.list
+
+import cl.arech.awesomeitems.products.data.ProductsDataRepository
+import cl.arech.awesomeitems.products.presentation.list.ListAction.LoadProductsAction
+import cl.arech.awesomeitems.products.presentation.list.ListResult.LoadProductsResult.Error
+import cl.arech.awesomeitems.products.presentation.list.ListResult.LoadProductsResult.InProgress
+import cl.arech.awesomeitems.products.presentation.list.ListResult.LoadProductsResult.Success
+import cl.arech.awesomeitems.products.presentation.list.mapper.ProductsMapper
+import cl.arech.mvi.execution.ExecutionThread
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onStart
+import javax.inject.Inject
+
+@FlowPreview
+class ListProcessor @Inject constructor(
+    private val repository: ProductsDataRepository,
+    private val mapper: ProductsMapper,
+    private val executionThread: ExecutionThread,
+) {
+    fun actionProcessor(action: ListAction): Flow<ListResult> =
+        when (action) {
+            is LoadProductsAction -> loadProductsProcessor(action.query)
+        }
+
+    private fun loadProductsProcessor(query: String) = flow<ListResult> {
+        repository.searchProducts(query).collect { remoteProducts ->
+            val products = with(mapper) { remoteProducts.toPresentation() }
+            emit(Success(products))
+        }
+    }.onStart {
+        emit(InProgress)
+    }.catch {
+        emit(Error)
+    }.flowOn(executionThread.ioThread())
+}
---
+++ app/src/main/java/cl/arech/awesomeitems/products/presentation/list/ListReducer.kt
@@ -0,0 +1,49 @@
+package cl.arech.awesomeitems.products.presentation.list
+
+import cl.arech.awesomeitems.products.presentation.list.ListResult.LoadProductsResult.Error
+import cl.arech.awesomeitems.products.presentation.list.ListResult.LoadProductsResult.InProgress
+import cl.arech.awesomeitems.products.presentation.list.ListResult.LoadProductsResult.Success
+import cl.arech.awesomeitems.products.presentation.list.ListUiState.DefaultUiState
+import cl.arech.awesomeitems.products.presentation.list.ListUiState.ErrorUiState
+import cl.arech.awesomeitems.products.presentation.list.ListUiState.LoadingUiState
+import cl.arech.awesomeitems.products.presentation.list.ListUiState.ShowProductsUiState
+import cl.arech.mvi.MviReducer
+import cl.arech.mvi.exception.UnsupportedReduceException
+import javax.inject.Inject
+
+class ListReducer @Inject constructor() : MviReducer<ListUiState, ListResult> {
+    override fun ListUiState.reduceWith(result: ListResult): ListUiState {
+        return when (val currentState = this) {
+            is DefaultUiState -> currentState reduceWith result
+            is LoadingUiState -> currentState reduceWith result
+            is ErrorUiState -> currentState reduceWith result
+            is ShowProductsUiState -> currentState reduceWith result
+        }
+    }
+
+    infix fun DefaultUiState.reduceWith(result: ListResult): ListUiState {
+        return when (result) {
+            InProgress -> LoadingUiState
+            else -> throw UnsupportedReduceException(this, result)
+        }
+    }
+
+    infix fun LoadingUiState.reduceWith(result: ListResult): ListUiState {
+        return when (result) {
+            Error -> ErrorUiState
+            is Success -> ShowProductsUiState(result.products)
+            else -> throw UnsupportedReduceException(this, result)
+        }
+    }
+
+    infix fun ErrorUiState.reduceWith(result: ListResult): ListUiState {
+        return when (result) {
+            InProgress -> LoadingUiState
+            else -> throw UnsupportedReduceException(this, result)
+        }
+    }
+
+    infix fun ShowProductsUiState.reduceWith(result: ListResult): ListUiState {
+        throw UnsupportedReduceException(this, result)
+    }
+}
