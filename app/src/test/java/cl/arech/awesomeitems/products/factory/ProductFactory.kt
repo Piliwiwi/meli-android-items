3f52db0f0208722976d2d226fe0c132c7bac941f
Piliwiwi <35037350+Piliwiwi@users.noreply.github.com>
2022-05-25 00:08:42 -0400

    test: products presentation mapper unit test

--- app/src/test/java/cl/arech/awesomeitems/products/factory/ProductFactory.kt
+++ app/src/test/java/cl/arech/awesomeitems/products/factory/ProductFactory.kt
@@ -3,6 +3,9 @@ package cl.arech.awesomeitems.products.factory
 import cl.arech.awesomeitems.products.data.remote.model.RemoteProduct
 import cl.arech.awesomeitems.products.data.remote.model.RemoteProducts
 import cl.arech.awesomeitems.products.data.remote.model.RemoteShipping
+import cl.arech.awesomeitems.products.presentation.list.model.Product
+import cl.arech.awesomeitems.products.presentation.list.model.Products
+import cl.arech.awesomeitems.products.presentation.list.model.Shipping
 import cl.arech.testingtools.factory.RandomFactory.generateBoolean
 import cl.arech.testingtools.factory.RandomFactory.generateInt
 import cl.arech.testingtools.factory.RandomFactory.generateString
@@ -20,6 +23,21 @@ object ProductFactory {
     )
 
     private fun makeRemoteShipping() = RemoteShipping(
+        hasFreeShipping = generateBoolean()
+    )
+
+    fun makeProducts(count: Int) = Products(
+        results = (0..count).map { makeProduct() }
+    )
+
+    private fun makeProduct() = Product(
+        title = generateString(),
+        price = generateInt(),
+        thumbnail = generateString(),
+        shipping = makeShipping()
+    )
+
+    private fun makeShipping() = Shipping(
         hasFreeShipping = generateBoolean()
     )
 }
---
+++ app/src/test/java/cl/arech/awesomeitems/products/presentation/list/mapper/ProductsMapperTest.kt
@@ -0,0 +1,39 @@
+package cl.arech.awesomeitems.products.presentation.list.mapper
+
+import cl.arech.awesomeitems.products.data.remote.model.RemoteProduct
+import cl.arech.awesomeitems.products.data.remote.model.RemoteShipping
+import cl.arech.awesomeitems.products.factory.ProductFactory.makeRemoteProducts
+import cl.arech.awesomeitems.products.presentation.list.model.Product
+import cl.arech.awesomeitems.products.presentation.list.model.Shipping
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+internal class ProductsMapperTest {
+    private val mapper = ProductsMapper()
+
+    @Test
+    fun `given RemoteProducts, when toPresentation, then Products`() {
+        val remote = makeRemoteProducts(6)
+
+        val presentation = with(mapper) {
+            remote.toPresentation()
+        }
+
+        remote.results?.zip(presentation.results)?.map {
+            assertProductEquals(it.first, it.second)
+        }
+    }
+
+    private fun assertProductEquals(remote: RemoteProduct?, presentation: Product) {
+        assertEquals(remote?.title.orEmpty(), presentation.title, "title")
+        assertEquals(remote?.price ?: 0, presentation.price, "price")
+        assertEquals(remote?.thumbnail.orEmpty(), presentation.thumbnail, "thumbnail")
+        assertShippingEquals(remote?.shipping, presentation.shipping)
+    }
+
+    private fun assertShippingEquals(remote: RemoteShipping?, presentation: Shipping) {
+        assertEquals(remote?.hasFreeShipping == true,
+            presentation.hasFreeShipping,
+            "hasFreeShipping")
+    }
+}
