1330b1e01589a3ca5f4378d09eff70b99b318b03
Piliwiwi <35037350+Piliwiwi@users.noreply.github.com>
2022-05-25 01:01:04 -0400

    feature: product list viewmodel

---
+++ app/src/main/java/cl/arech/awesomeitems/products/presentation/ListViewModel.kt
@@ -0,0 +1,57 @@
+package cl.arech.awesomeitems.products.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cl.arech.awesomeitems.products.presentation.list.ListAction.LoadProductsAction
+import cl.arech.awesomeitems.products.presentation.list.ListProcessor
+import cl.arech.awesomeitems.products.presentation.list.ListReducer
+import cl.arech.awesomeitems.products.presentation.list.ListUIntent
+import cl.arech.awesomeitems.products.presentation.list.ListUIntent.RetrySearchUIntent
+import cl.arech.awesomeitems.products.presentation.list.ListUIntent.SearchProductsInitialUIntent
+import cl.arech.awesomeitems.products.presentation.list.ListUiState
+import cl.arech.awesomeitems.products.presentation.list.ListUiState.DefaultUiState
+import cl.arech.mvi.MviPresentation
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.scan
+import javax.inject.Inject
+
+@FlowPreview
+@ExperimentalCoroutinesApi
+class ListViewModel @Inject constructor(
+    private val processor: ListProcessor,
+    private val reducer: ListReducer,
+) : ViewModel(),
+    MviPresentation<ListUIntent, ListUiState> {
+    private val defaultUiState: ListUiState = DefaultUiState
+    private val uiState: MutableStateFlow<ListUiState> = MutableStateFlow(defaultUiState)
+
+    override fun processUserIntents(userIntents: Flow<ListUIntent>) {
+        userIntents
+            .buffer()
+            .flatMapMerge { userIntent ->
+                processor.actionProcessor(userIntent.toAction())
+            }
+            .scan(defaultUiState) { currentUiState, result ->
+                with(reducer) { currentUiState reduceWith result }
+            }
+            .onEach { uiState ->
+                this.uiState.value = uiState
+            }
+            .launchIn(viewModelScope)
+    }
+
+    private fun ListUIntent.toAction() =
+        when (this) {
+            is SearchProductsInitialUIntent -> LoadProductsAction(query)
+            is RetrySearchUIntent -> LoadProductsAction(query)
+        }
+
+    override fun uiStates() = uiState
+}
--- mvi/src/main/java/cl/arech/mvi/MviPresentation.kt
+++ mvi/src/main/java/cl/arech/mvi/MviPresentation.kt
@@ -3,7 +3,9 @@ package cl.arech.mvi
 import cl.arech.mvi.events.MviUiState
 import cl.arech.mvi.events.MviUserIntent
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
 
 interface MviPresentation<TUserIntent : MviUserIntent, TUiState : MviUiState> {
-    fun processUserIntentsAndObserveUiStates(userIntents: Flow<TUserIntent>): Flow<TUiState>
+    fun processUserIntents(userIntents: Flow<TUserIntent>)
+    fun uiStates(): StateFlow<TUiState>
 }
