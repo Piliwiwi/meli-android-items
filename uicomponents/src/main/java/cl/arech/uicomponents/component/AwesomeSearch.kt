35d4836bf91d555e1a0e687a6752baa371bcbeab
Piliwiwi <35037350+Piliwiwi@users.noreply.github.com>
2022-05-26 18:47:46 -0400

    feature: awesome search component events

--- uicomponents/src/main/java/cl/arech/uicomponents/component/AwesomeSearch.kt
+++ uicomponents/src/main/java/cl/arech/uicomponents/component/AwesomeSearch.kt
@@ -4,8 +4,14 @@ import android.content.Context
 import android.content.Context.LAYOUT_INFLATER_SERVICE
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.SearchView.OnQueryTextListener
 import androidx.constraintlayout.widget.ConstraintLayout
 import cl.arech.uicomponents.databinding.UiComponentAwesomeSearchBinding
+
+data class AttrsAwesomeSearch(
+    val onSubmit: (String) -> Unit = {},
+    val onChange: (String) -> Unit = {},
+)
 
 class AwesomeSearch @JvmOverloads constructor(
     context: Context,
@@ -19,5 +25,23 @@ class AwesomeSearch @JvmOverloads constructor(
             val inflater = context.getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
             binding = UiComponentAwesomeSearchBinding.inflate(inflater, this)
         }
+    }
+
+    fun setAttributes(attrs: AttrsAwesomeSearch) {
+        setSearchEvents(attrs)
+    }
+
+    private fun setSearchEvents(attrs: AttrsAwesomeSearch) = binding?.apply {
+        awesomeSearchView.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (!query.isNullOrEmpty()) attrs.onSubmit(query)
+                return false
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                attrs.onChange(query.orEmpty())
+                return false
+            }
+        })
     }
 }
