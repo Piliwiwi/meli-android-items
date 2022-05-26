683010796452daf92cca716407104aae298efbcd
Piliwiwi <35037350+Piliwiwi@users.noreply.github.com>
2022-05-26 18:50:41 -0400

    feature: navigate to products from main

--- app/src/main/java/cl/arech/awesomeitems/MainActivity.kt
+++ app/src/main/java/cl/arech/awesomeitems/MainActivity.kt
@@ -2,10 +2,17 @@ package cl.arech.awesomeitems
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cl.arech.awesomeitems.products.ui.ProductsActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigateToProducts()
+    }
+
+    private fun navigateToProducts() {
+        val intent = ProductsActivity.makeIntent(this)
+        startActivity(intent)
     }
 }
