1ab6bfaa79877f44554cc2ef76c27a0366e3224e
Piliwiwi <35037350+Piliwiwi@users.noreply.github.com>
2022-05-26 22:07:56 -0400

    feature: adding execution thread dependency

--- app/src/main/java/cl/arech/awesomeitems/common/di/ApplicationModule.kt
+++ app/src/main/java/cl/arech/awesomeitems/common/di/ApplicationModule.kt
@@ -2,20 +2,24 @@ package cl.arech.awesomeitems.common.di
 
 import android.content.Context
 import cl.arech.awesomeitems.common.factory.NetworkDependenciesFactory
+import cl.arech.mvi.execution.AppExecutionThread
+import cl.arech.mvi.execution.ExecutionThread
 import cl.arech.network.config.NetworkDependencies
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class ApplicationModule {
     @Provides
-    @Singleton
     fun providesNetWorkDependencies(
-        context: Context,
+        @ApplicationContext context: Context,
     ): NetworkDependencies =
         NetworkDependenciesFactory.makeNetworkDependencies(context)
+
+    @Provides
+    fun providesExecutionThread(): ExecutionThread = AppExecutionThread()
 }
