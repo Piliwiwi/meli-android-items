91bb3effb14af6d8068cc722926b0d0b70f3ac17
Piliwiwi <35037350+Piliwiwi@users.noreply.github.com>
2022-05-28 22:03:35 -0400

    improve: amountformatter

--- utils/src/main/java/cl/arech/utils/formatter/AmountFormatter.kt
+++ utils/src/main/java/cl/arech/utils/formatter/AmountFormatter.kt
@@ -1,18 +1,8 @@
 package cl.arech.utils.formatter
 
-fun Int.toAmountFormat(): String {
-    val stringAmount = toString()
-    return "$${
-        when {
-            stringAmount.length <= 3 -> stringAmount
-            stringAmount.length % 3 == 0 -> stringAmount
-                .chunked(3)
-                .joinToString(".")
-            else -> stringAmount
-                .reversed()
-                .chunked(3)
-                .joinToString(".")
-                .reversed()
-        }
-    }"
+import java.text.DecimalFormat
+
+fun Double.toAmountFormat(): String {
+    val dec = DecimalFormat("#,###.##")
+    return "$${dec.format(this)}"
 }
